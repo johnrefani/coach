f9384ca7c31b43f70138fb8e698d0060425ed09f
John Cloyd Refani <jhnrefani@gmail.com>
2025-09-09 16:22:08 +0800

    Linked to Forums

--- mentee/sessions.php
+++ mentee/sessions.php
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `session_bookings` (
           <li><a href="resourcelibrary.php">Resource Library</a></li>
           <li><a href="activities.php">Activities</a></li>
           <li><a href="forum-chat.php">Sessions</a></li>
-          <li><a href="group-chat.php">Forums</a></li>
+          <li><a href="forums.php">Forums</a></li>
         </ul>
       </div>
 
@@ -650,7 +650,7 @@ $notifCount = $notifResult->fetch_assoc()[\'count\'];
                     <li><a href="CoachMenteeHome.php#resourcelibrary">Resource Library</a></li>
                     <li><a href="CoachMenteeHome.php#mentors">Activities</a></li>
                     <li><a href="forum-chat.php">Sessions</a></li>
-                    <li><a href="group-chat.php">Forums</a></li>
+                    <li><a href="forums.php">Forums</a></li>
                 </ul>
             </div>
 
