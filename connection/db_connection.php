d9be09be27a683ff7572e01767b2bff87c495e69
Johnrey <160460651+johnreyluntayao@users.noreply.github.com>
2025-09-22 02:02:41 +0800

    Update db_connection.php

--- connection/db_connection.php
+++ connection/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "coach-hub");
+$conn = new mysqli("localhost", "coach-user", "coach2025Hub!", "coach-hub");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
