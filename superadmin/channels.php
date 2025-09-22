be975848784993e86de8d423c16362e327907ea3
Mikaela Cando <mntcando@gmail.com>
2025-09-22 22:10:55 +0800

    Fixing Dashboard

--- superadmin/channels.php
+++ superadmin/channels.php
@@ -239,7 +239,7 @@ foreach ($forums as $forum) {
   <div class="menu-items">
         <ul class="navLinks">
             <li class="navList"><a href="dashboard.php"><ion-icon name="home-outline"></ion-icon><span class="links">Home</span></a></li>
-            <li class="navList"><a href="moderators.php"><ion-icon name="lock-closed-outline"></ion-icon><span class="links">Moderators</span></a>ss</li>
+            <li class="navList"><a href="moderators.php"><ion-icon name="lock-closed-outline"></ion-icon><span class="links">Moderators</span></a></li>
             <li class="navList"><a href="courses.php"><ion-icon name="book-outline"></ion-icon><span class="links">Courses</span></a></li>
             <li class="navList"><a href="manage_mentees.php"><ion-icon name="person-outline"></ion-icon><span class="links">Mentees</span></a></li>
             <li class="navList"><a href="manage_mentors.php"><ion-icon name="people-outline"></ion-icon><span class="links">Mentors</span></a></li>
