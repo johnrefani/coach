f29398e22be81c1e5e903ee41b0c899427bd6aa9
Mikaela Cando <mntcando@gmail.com>
2025-09-24 16:05:01 +0800

    Checking Errors

--- admin/courses.php
+++ admin/courses.php
@@ -1,5 +1,7 @@
 <?php
-session_start(); 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
 
 // Standard session check for an admin user
 if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'Admin') {
