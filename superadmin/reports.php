59848250215d823c50a3658426d01a30b6c7a85d
John Kenneth Dizon <johnkennethdizon16@gmail.com>
2025-09-27 03:31:23 +0800

    Changing title

--- superadmin/reports.php
+++ superadmin/reports.php
@@ -125,7 +125,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Super Admin - Reported Content</title>
+    <title>Reported Content | SuperAdmin</title>
      <link rel="icon" href="../uploads/img/coachicon.svg" type="image/svg+xml">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
