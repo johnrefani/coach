ddae241418c4573930038368ab07e9fc5e2f775c
John Kenneth Dizon <johnkennethdizon16@gmail.com>
2025-09-30 16:41:22 +0800

    Increased Size of Avatar

--- mentee/forums.php
+++ mentee/forums.php
@@ -650,7 +650,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'get_contributors') {
               $firstName = $row['first_name'] ?? '';
               $lastName = $row['last_name'] ?? '';
               
-              $avatarSize = '25px'; 
+              $avatarSize = '75px'; 
               $avatarMargin = '4px'; // Compact spacing
               $likerAvatar = '';
               
