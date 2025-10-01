60e4972b699ebf295296bca4f796540a9a86cd0e
John Kenneth Dizon <johnkennethdizon16@gmail.com>
2025-10-01 21:09:49 +0800

    fix

--- superadmin/feedbacks.php
+++ superadmin/feedbacks.php
@@ -144,7 +144,7 @@ header h1 {
 
 
 .bottom-link {
-    margin-top: 9px;
+    margin-top: -5px;
     border-top: 1px solid rgba(255, 255, 255, 0.1);
     /* ADDED: Ensures the bottom of the link is not cut off by the viewport edge */
     padding-bottom: -20px; 
