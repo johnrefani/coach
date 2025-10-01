9009de61aff21b92e5b207f9d2136820510830c4
John Kenneth Dizon <johnkennethdizon16@gmail.com>
2025-10-01 17:24:05 +0800

    fix

--- admin/manage_mentors.php
+++ admin/manage_mentors.php
@@ -635,12 +635,6 @@ $conn->close();
           </a>
         </li>
         <li class="navList">
-          <a href="moderators.php">
-            <ion-icon name="lock-closed-outline"></ion-icon>
-            <span class="links">Moderators</span>
-          </a>
-        </li>
-        <li class="navList">
             <a href="manage_mentees.php"> <ion-icon name="person-outline"></ion-icon>
               <span class="links">Mentees</span>
             </a>
@@ -680,16 +674,8 @@ $conn->close();
               <span class="links">Resource Library</span>
             </a>
         </li>
-        <li class="navList">
-            <a href="reports.php"><ion-icon name="folder-outline"></ion-icon>
-              <span class="links">Reported Posts</span>
-            </a>
-        </li>
-        <li class="navList">
-            <a href="banned-users.php"><ion-icon name="person-remove-outline"></ion-icon>
-              <span class="links">Banned Users</span>
-            </a>
-        </li>
+     
+        
       </ul>
 
    <ul class="bottom-link">
