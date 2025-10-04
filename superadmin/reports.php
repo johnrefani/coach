7eb62af1e339dd2b5357364860133b98a09f8c0a
John Kenneth Dizon <johnkennethdizon16@gmail.com>
2025-10-05 00:00:51 +0800

    fix

--- superadmin/reports.php
+++ superadmin/reports.php
@@ -162,6 +162,7 @@ $conn->close();
                     <span class="links">Home</span>
                 </a>
             </li>
+            <li class="navList"><a href="moderators.php"><ion-icon name="lock-closed-outline"></ion-icon><span class="links">Moderators</span></a></li>
 
             <li class="navList">
                 <a href="manage_mentees.php"> <ion-icon name="person-outline"></ion-icon>
