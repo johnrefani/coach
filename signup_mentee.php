5d840a67192b6628d13affccfa0c09d693b397bd
John Kenneth Dizon <johnkennethdizon16@gmail.com>
2025-10-13 23:23:54 +0800

    fix

--- signup_mentee.php
+++ signup_mentee.php
@@ -359,19 +359,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST['check_username'])) {
         <input type="hidden" id="tech-interests" name="learning" value="">
                 </div>
                 <div class="terms-container">
-                    <label>
-                        <input type="checkbox" id="terms" required>
-                        <span>
-                            I agree to the <a href="#">Terms & Conditions</a> and <a href="#">Data Privacy Policy</a>.
-                        </span>
-                    </label>
-                    <label>
-                        <input type="checkbox" id="consent" required>
-                        <span>
-                            I consent to receive updates and communications from COACH, trusting that all shared information will be used responsibly to support my growth and development. I understand that COACH values my privacy and that I can opt out of communications at any time.
-                        </span>
-                    </label>
-                </div>
+    <label>
+        <input type="checkbox" id="terms" required>
+        <span>
+            I agree to the <a href="termscondition.php">Terms & Conditions</a> and <a href="dataprivacy.php">Data Privacy Policy</a>.
+        </span>
+    </label>
+    <label>
+        <input type="checkbox" id="consent" required>
+        <span>
+            I consent to receive updates and communications from COACH, trusting that all shared information will be used responsibly to support my growth and development. I understand that COACH values my privacy and that I can opt out of communications at any time.
+        </span>
+    </label>
+</div>
                 <div class="form-buttons">
                     <button type="button" class="cancel-btn"><a href="login.php" style="color: #290c26;">Cancel</a></button>
                     <button type="submit" class="submit-btn">Register</button>
