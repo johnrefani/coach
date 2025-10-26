c76a70a15933721d7fbf09fc7483b0c78e415077
Mikaela Cando <mntcando@gmail.com>
2025-10-26 16:28:38 +0800

    Fix

--- superadmin/report_generation.php
+++ superadmin/report_generation.php
@@ -301,12 +301,11 @@ $contributors = $result_contributors->fetch_all(MYSQLI_ASSOC);
             <div class="logo-image1"><img src="../uploads/img/coach3d.png" alt="Logo"></div>
             <div class="logo-name1">COACH Report Analysis</div>
             </div>
-
             <div style="margin: 20px 0; text-align: right;">
         <button id="save-pdf" class="btn">Save Report as PDF</button>
     </div>
     
-        <form method="POST" style="display:inline;">
+    <form method="POST" style="margin: 20px 0; text-align: right;">
             <input type="text" name="daterange" class="date-range" value="16 Mar 2020 - 21 Mar 2020" />
         </form>
     </div>
